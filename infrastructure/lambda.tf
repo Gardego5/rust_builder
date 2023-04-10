6881eda5a9e8981efba0b6e56fdf1c6e84422d50
Garrett Davis <garrett.davis.7777@gmail.com>
2023-04-09 18:15:00 -0700

    lambda builds

---
+++ infrastructure/lambda.tf
@@ -0,0 +1,37 @@
+data "archive_file" "image_lambda" {
+  type        = "zip"
+  source_dir  = "../bin/image_lambda"
+  output_path = "image_lambda.zip"
+}
+
+resource "aws_lambda_function" "image_lambda" {
+  filename         = data.archive_file.image_lambda.output_path
+  function_name    = "${var.app_prefix}-image-lambda"
+  role             = aws_iam_role.image_lambda.arn
+  handler          = "bootstrap"
+  source_code_hash = data.archive_file.image_lambda.output_base64sha256
+  architectures    = ["arm64"]
+  runtime          = "provided.al2"
+}
+
+resource "aws_lambda_function_url" "image_lambda" {
+  function_name      = aws_lambda_function.image_lambda.function_name
+  authorization_type = "NONE"
+}
+
+data "aws_iam_policy_document" "image_lambda" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+resource "aws_iam_role" "image_lambda" {
+  name               = "${var.app_prefix}-image-lambda"
+  assume_role_policy = data.aws_iam_policy_document.image_lambda.json
+}
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,3 +1,22 @@
 resource "aws_s3_bucket" "image_bucket" {
-  bucket_prefix = "${var.app_prefix}-image-bucket"
+  bucket = "${var.app_prefix}-image-bucket"
+}
+
+data "aws_iam_policy_document" "image_bucket_read" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      aws_s3_bucket.image_bucket.arn,
+      "${aws_s3_bucket.image_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "image_bucket_read" {
+  policy = data.aws_iam_policy_document.image_bucket_read.json
+  name   = "${var.app_prefix}-image-bucket-read"
 }
